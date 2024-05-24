55e9cf3e6e96d7f6d3641ccccaed464fa6c26857
jp1593 <juanpablo08082002@hotmail.com>
2024-05-24 00:08:10 -0600

    Font project modified

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Header from "../src/components/Header";
+import "./App.css";
 
 function App() {
   return (
-    <div className="bg-black">
+    <div className="bg-black arvo-regular">\
       <Header />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,19 +4,19 @@ function Header() {
   return (
     <header>
       <div className="flex item-center justify-center py-10">
-        <div className="container text-white border rounded-full">
+        <div className="containerh text-white border rounded-full">
           <nav className="justify-between items-center h-10 p-10 hidden sm:flex">
             <ul className="flex gap-10">
-              <li className="hover:text-gray-500">
+              <li>
                 <a href="/experience">Experience</a>
               </li>
-              <li className="hover:text-gray-500">
+              <li>
                 <a href="/projects">Projects</a>
               </li>
-              <li className="hover:text-gray-500">
+              <li>
                 <a href="/stack">Stack</a>
               </li>
-              <li className="hover:text-gray-500">
+              <li>
                 <a href="/contact">Contact</a>
               </li>
             </ul>
