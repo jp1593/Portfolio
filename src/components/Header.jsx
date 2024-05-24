ee65a59881da12ecb3dfa5cc13c9973cb7259827
jp1593 <juanpablo08082002@hotmail.com>
2024-05-23 23:49:34 -0600

    Changes on header hover

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
       <div className="flex item-center justify-center py-10">
         <div className="container text-white border rounded-full">
           <nav className="justify-between items-center h-10 p-10 hidden sm:flex">
-            <ul className="flex gap-10 ul">
+            <ul className="flex gap-10">
               <li className="hover:text-gray-500">
                 <a href="/experience">Experience</a>
               </li>
