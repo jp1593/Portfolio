b72331ca4a909a1076480e17d89fcdc18d0fa8d1
jp1593 <juanpablo08082002@hotmail.com>
2024-05-27 20:27:45 -0600

    Owner and Social added Css & JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "../src/components/Header";
+import Owner from "../src/components/Owner"; 
 import "./App.css";
 
 function App() {
   return (
     <div className="bg-black arvo-regular">\
       <Header />
+      <Owner />
     </div>
   );
 }
