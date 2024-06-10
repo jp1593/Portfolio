927c938e209d8132dc9b26c097d71fd3af0c6a1f
jp1593 <juanpablo08082002@hotmail.com>
2024-06-09 20:03:00 -0600

    Added transtions and css styles

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -1,28 +1,28 @@
 import "./Stack.css";
-import HTML from "../assets/html-1.svg"
-import CSS from "../assets/css-3.svg"
-import React from "../assets/React-Logo.svg"
-import Python from "../assets/python-svgrepo-com.svg"
-import JS from "../assets/logo-javascript.svg"
-import MYSQL from "../assets/mysql-ar21.svg"
-import Node from "../assets/nodejs-icon.svg"
-import git from "../assets/git-icon-logo-svgrepo-com.svg"
-import TailWind from "../assets/tailwind-svgrepo-com.svg"
-import Svelte from "../assets/svelte-1.svg"
+import HTML from "../assets/html-1.svg";
+import CSS from "../assets/css-3.svg";
+import React from "../assets/React-Logo.svg";
+import Python from "../assets/python-5.svg";
+import JS from "../assets/logo-javascript.svg";
+import MYSQL from "../assets/mysql-ar21.svg";
+import Node from "../assets/nodejs-icon.svg";
+import git from "../assets/git-icon-logo-svgrepo-com.svg";
+import TailWind from "../assets/tailwind-svgrepo-com.svg";
+import Svelte from "../assets/svelte-1.svg";
 
 export default function Stack() {
-    return( 
-    <div className="logos-slide"> 
-        <img src={HTML}/> 
-        <img src={CSS}/> 
-        <img src={React}/> 
-        <img src={Svelte}/> 
-        <img src={JS}/> 
-        <img src={TailWind}/> 
-        <img src={Python}/> 
-        <img src={MYSQL}/> 
-        <img src={Node}/> 
-        <img src={git}/> 
-
+  return (
+    <div className="flex logos-slide overflow-hidden mt-10">
+      <img className="logos" src={HTML} />
+      <img className="logos" src={CSS} />
+      <img className="logos" src={React} />
+      <img className="logos" src={Svelte} />
+      <img className="logos" src={JS} />
+      <img className="logos" src={TailWind} />
+      <img className="logos" src={Python} />
+      <img className="logos" src={MYSQL} />
+      <img className="logos" src={Node} />
+      <img className="logos" src={git} />
     </div>
-)}
+  );
+}
